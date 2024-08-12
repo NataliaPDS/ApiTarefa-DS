afbd4de903c69f75535e090312536033812fb23f
Natalia Pereira da Silva <2022102020084@aluno.ifro.edu.br>
2024-08-12 16:56:12 -0400

    primeiro

---
+++ API - Tarefa1/API - Tarefa1/Controllers/PrincipalController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace API___Tarefa1.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class PrincipalController : ControllerBase
+    {
+
+        [HttpGet("/")]
+        public IActionResult Get()
+        {
+            return Ok("API Tarefas: online");
+        }
+
+        [HttpGet("/hello-world")]
+        public IActionResult GetHelloWorld()
+        {
+            return Ok("Hello World Natalia");
+        }
+
+        [HttpGet("autor")]
+        public IActionResult GetAutor()
+        {
+            return Ok("Desenvolvimento de Natalia");
+        }
+
+    }
+}
---
+++ API - Tarefa1/API - Tarefa1/Controllers/TarefaController.cs
@@ -0,0 +1,55 @@
+﻿using API___Tarefa1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API___Tarefa1.Controllers
+{
+    [Route("Tarefas")]
+    [ApiController]
+    public class TarefasController : ControllerBase
+    {
+        List<Tarefa> listaTarefas = new List<Tarefa>();
+
+        public TarefasController() 
+        {
+   
+            var tarefa1 = new Tarefa()
+            {
+                Id = 1,
+                Descricao = "Estudo do API 1 Parte",
+            };
+
+            var tarefa2 = new Tarefa()
+            {
+                Id = 2,
+                Descricao = "Estudo do API 2 Parte",
+            };
+
+            var tarefa3 = new Tarefa()
+            {
+                Id = 3,
+                Descricao = "Estudo do API 3 Parte",
+            };
+            listaTarefas.Add(tarefa1);
+            listaTarefas.Add(tarefa2);
+            listaTarefas.Add(tarefa3);
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(listaTarefas);
+            
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var tarefa = listaTarefas.Where (item => item.Id == id).FirstOrDefault();
+
+            return Ok(tarefa);
+        }
+
+    }
+}
